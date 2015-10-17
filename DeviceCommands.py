6afb4aabc4a3d00879d677f34d7c946e22bc2e90
Matthew Gotteiner <matthew.gotteiner@gmail.com>
2015-10-17 11:59:13 -0400

    split device commands from interpreter

---
+++ DeviceCommands.py
@@ -0,0 +1,24 @@
+class LedSet:
+	def __init__(self, device_name, val):
+		self.device_name = device_name
+		self.val = val
+	def interp(self):
+		led = devices.get_out(self.device_name)
+		if not isinstance(led, devices.Led):
+			devices.error('device {} is the wrong type'.format(led))
+		led.set(self.val.interp())
+	def __repr__(self):
+		return "Led {} Set {}".format(self.device_name, self.val.__repr__())
+
+class WaitButtonPress:
+	def __init__(self, device_name):
+		self.device_name = device_name
+	def interp(self):
+		button = devices.get_in(self.device_name)
+		if not isinstance(button, devices.Button):
+			devices.error('device {} is the wrong type'.format(button))
+		button.wait_for_press()
+	def __repr__(self):
+		return 'Wait Button {} Press'.format(self.device_name)
+
+
--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,6 @@
 import json, time, devices
 import RPi.GPIO as GPIO
+import DeviceCommands as *
 
 class If:
 	def __init__(self, cond, page_name):
@@ -46,29 +47,6 @@ class Constant:
 		return self.constant
 	def __repr__(self):
 		return self.constant.__repr__()
-
-class LedSet:
-	def __init__(self, device_name, val):
-		self.device_name = device_name
-		self.val = val
-	def interp(self):
-		led = devices.get_out(self.device_name)
-		if not isinstance(led, devices.Led):
-			devices.error('device {} is the wrong type'.format(led))
-		led.set(self.val.interp())
-	def __repr__(self):
-		return "Led {} Set {}".format(self.device_name, self.val.__repr__())
-
-class WaitButtonPress:
-	def __init__(self, device_name):
-		self.device_name = device_name
-	def interp(self):
-		button = devices.get_in(self.device_name)
-		if not isinstance(button, devices.Button):
-			devices.error('device {} is the wrong type'.format(button))
-		button.wait_for_press()
-	def __repr__(self):
-		return 'Wait Button {} Press'.format(self.device_name)
 
 class Print:
 	def __init__(self, param):
