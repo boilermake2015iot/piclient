9d6368bee33f0b480c1e91799cc65b5b44cca96f
Matthew Gotteiner <matthew.gotteiner@gmail.com>
2015-10-17 02:31:27 -0400

    simple interpreter

--- interpreter.py
+++ interpreter.py
@@ -5,15 +5,14 @@ class Device:
 		self.channel = channel
 
 class If:
-	def __init__(self, cond, true_block, false_block):
+	def __init__(self, cond, true_node):
 		self.cond = cond
-		self.true_block = true_block
-		self.false_block = false_block
-	def interp():
+		self.true_node = true_node
+	def interp(self):
 		if (self.cond.interp()):
-			self.true_block.interp()
-		else:
-			self.false_block.interp()
+			self.true_node.interp()
+	def __repr__(self):
+		return "if {}: {} else: {}".format(self.cond.__repr__(), self.true_node.__repr__(), self.cond.false_node.__repr__())
 
 opToFn = {
 	'+': lambda lhs, rhs: lhs + rhs,
@@ -32,17 +31,97 @@ class Expression:
 	def __init__(self, op, lhs, rhs):
 		if op not in opToFn:
 			raise "Unknown operator {}".format(op)
+		self.op_str = op
 		self.op = opToFn[op]
 		self.lhs = lhs
 		self.rhs = rhs
-	def interp():
+	def interp(self):
 		return self.op(lhs.interp(), rhs.interp())
+	def __repr__(self):
+		return "{} {} {}".format(self.lhs.__repr__(), self.op_str, self.rhs.__repr__())
 
 class Constant:
 	def __init__(self, constant):
 		self.constant = constant
-	def interp():
+	def interp(self):
 		return self.constant
-	
-class Page:
+	def __repr__(self):
+		return self.constant.__repr__()
 
+class Print:
+	def __init__(self, param):
+		self.param = param
+	def interp(self):
+		print self.param.interp()
+	def __repr__(self):
+		return "print({})".format(self.param.__repr__())
+
+class PageDecl:
+	def __init__(self, name, nodes):
+		self.name = name
+		self.nodes = nodes
+	def interp(self):
+		for node in self.nodes:
+			node.interp()
+	def __repr__(self):
+		print self.nodes.__repr__()
+		return "Name: {}, Nodes: {}".format(self.name, self.nodes.__repr__())
+
+def translate_error(msg, node):
+	raise msg.format(json.dumps(node))
+
+def translate_constant(node):
+	if 'Value' not in node:
+		translate_error('Malformed constant {}', node)
+	return Constant(node['Value'])
+
+def translate_expression(node):
+	if node['Type']== 'Constant':
+		return translate_constant(node)
+	elif node['Type'] == 'Expression':
+		if 'Left' not in node or 'Right' not in node:
+			translate_error('Malformed expression {}', node)
+		return Expression(node.Op, translate_expression(node['Left']), translate_expression(node['Right']))
+	else:
+		translate_error('Invalid expression node {}', node)
+
+def translate_if(node):
+	if 'Condition' not in node or 'Page' not in node:
+		translate_error('Malformed expression {}', node)
+	return If(translate_expression(node['Condition']), translate_page(node['Page']))
+
+def translate_print(node):
+	if 'Param' not in node:
+		translate_error('Malformed print {}', node)
+	return Print(translate_expression(node['Param']))
+
+def translate_page_decl(page):
+	if 'Name' not in page or 'Nodes' not in page:
+		translate_error('Malformed page {}', page)
+	return PageDecl(page['Name'], translate_nodes(page['Nodes']))
+	
+def translate_nodes(nodes):
+	translated = []
+	for node in nodes:
+		if 'Type' not in node:
+			translate_error('Malformed node {}', node)
+		if node['Type'] == 'If':
+			translated.append(translate_expression(node))
+		elif node['Type'] == 'Print':
+			translated.append(translate_print(node))
+		else:
+			translate_error('Malformed node {}', node)
+	return translated
+
+def interp(doc):
+	if 'Pages' not in doc:
+		raise "Malformed doc {}".format(json.dumps(doc))
+	page_decls = {}
+	for page_doc in doc['Pages']:
+		page_decl = translate_page_decl(page_doc)
+		page_decls[page_decl.name] = page_decl
+	if 'Main' not in page_decls:
+		raise 'Malformed doc - no main {}'.format(json.dumps(doc))
+	page_decls['Main'].interp()
+
+interp({'Pages': [{'Name': 'Main', 'Nodes': [{'Type': 'Print', 'Param': {'Type': 'Constant', 'Value': 5}}]}]})
